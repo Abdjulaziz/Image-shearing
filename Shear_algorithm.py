844048d9a20ba63e78e78c3f83bf25525172f0c6
Caseguy <33653535+Caseguy@users.noreply.github.com>
2018-10-31 07:41:09 +0100

    Few changes to make the code more understandble, And sheared images.

--- Shear_algorithm.py
+++ Shear_algorithm.py
@@ -1,10 +1,12 @@
 import cv2
 import numpy as np
 
-# Basic image read
+# Basic image reading
 img = cv2.imread("2.PNG", 0)
+# Shearing factors in X and Y direction
 Bx = 1
 By = 1
+#imread rows and columns
 rows = img.shape[0]
 cols = img.shape[1]
 
@@ -17,31 +19,31 @@ CopyOf_img4 = np.zeros((rows, cols + rows, 3), np.uint8)
 
 def shearAlgorithm():
     # ShearY algorithm
-    for i in range(0, img.shape[0]):
-        for j in range(0, img.shape[1]):
+    for i in range(0, rows):
+        for j in range(0, cols):
             n = By * j
             k = img[i, j]
 
             if i >= 0 and i <= rows and j >= 0 and j <= cols:
                 CopyOf_img1[i + n, j] = k
 
-    for i in range(0, img.shape[0]):
-        for j in range(0, img.shape[1]):
+    for i in range(0, rows):
+        for j in range(0, cols):
             n = By * j
             k = img[i, j]
             if i >= 0 and i <= rows and j >= 0 and j <= cols:
                 CopyOf_img2[i - n + cols, j] = k
 
     # ShearX algorithm
-    for i in range(0, img.shape[0]):
-        for j in range(0, img.shape[1]):
+    for i in range(0, rows):
+        for j in range(0, cols):
             n = Bx * i
             k = img[i, j]
             if i >= 0 and i <= rows and j >= 0 and j <= cols:
                 CopyOf_img3[i, j + n] = k
 
-    for i in range(0, img.shape[0]):
-        for j in range(0, img.shape[1]):
+    for i in range(0, rows):
+        for j in range(0, cols):
             n = Bx * i
             k = img[i, j]
             if i >= 0 and i <= rows and j >= 0 and j <= cols:
@@ -50,21 +52,21 @@ def shearAlgorithm():
     # Default image
     cv2.imshow("defult", img)
 
-    # ShearX
+    # show shearY
     cv2.imshow("shearY1", CopyOf_img1)
     cv2.imshow("shearY2", CopyOf_img2)
 
-    # ShearY
+    # Show shearX
     cv2.imshow("shearX1", CopyOf_img3)
     cv2.imshow("shearX2", CopyOf_img4)
 
-    # Write images
+    # Writes images to folder and saves them
     cv2.imwrite('shearY1.PNG', CopyOf_img1)
     cv2.imwrite('shearY2.PNG', CopyOf_img2)
     cv2.imwrite('shearX1.PNG', CopyOf_img3)
     cv2.imwrite('shearX2.PNG', CopyOf_img4)
 
-    # Any key can be pressed to destroy the image
+    # Any key can be pressed to destroy the image and exit out
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
